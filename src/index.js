b7dd2d4d0057d4c3604d05b8a06e1149dccedab2
DARSHIKA91 <darshikaagrawal9175@gmail.com>
2024-07-19 16:53:43 +0530

    add

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from "./views/Home/Home"
-
+import AddPlant from "./views/AddPlant/AddPlant"
+import UpdatePlant from './views/UpdatePlant/UpdatePlant';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>
+    element: <Home />
+  },
+  {
+    path: "/add",
+    element: <AddPlant/>
+  },
+  {
+    path: "/update/:id",
+    element: <UpdatePlant/>
   },
   {
     path: "*",
@@ -18,5 +28,4 @@ const router = createBrowserRouter([
 
 root.render((<div>
   <RouterProvider router={router}/>
-  </div>));
-
+</div>));
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,50 +1,60 @@
-import React, { useEffect, useState } from 'react'
 import "./Home.css"
-import PlantCard from '../../component/PlantCard/PlantCard'
-import axios from 'axios'
-import toast, {Toaster} from 'react-hot-toast'
+import React, { useEffect, useState } from 'react'
+import PlantCard from "./../../component/PlantCard/PlantCard"
+import axios from 'axios';
+import toast, {Toaster} from "react-hot-toast"
+import ImgAdd from "./add.png"
+import { Link } from "react-router-dom"
 
 function Home() {
-    const [plants, setPlants] = useState([])
+  const [plants, setPlants] = useState([])
 
-    const loadPlants = async ()=>{
-        toast.loading("Loding plants...")
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/plants`)
-        toast.dismiss()
-        toast.success("Plants loaded successfully")
-        setPlants(response.data.data)
-    }
+  const loadPlants = async () => {
+    toast.loading("Loading plants...")
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/plants`)
 
-    useEffect(()=>{
-        loadPlants()
-    }, [])
+    toast.dismiss()
+    toast.success("Plants loaded successfully")
+
+    setPlants(response.data.data)
+  }
+
+  useEffect(()=>{
+    loadPlants()
+  }, [])
 
   return (
     <div>
-        <h1>Plants</h1>
+      <h1>Plants</h1>
 
-        {
-            plants.map((plants, i)=>{
-                const {
-                    _id,
-                    name, 
-                    category, 
-                    image, 
-                    price, 
-                    description} = plants
+      {
+        plants.map((plant, i)=>{
+          const {
+            _id, 
+            name, 
+            category, 
+            price, 
+            image, 
+            description
+          } = plant
 
-                return <PlantCard 
-                key={i}
-                _id={_id} 
-                name={name} 
-                category={category} 
-                image={image} 
-                price={price} 
-                description={description}/>
-            })
-        }
-        <Toaster/>
-    </div>
+          return (<PlantCard 
+              key={i}
+              _id={_id} 
+              name={name} 
+              category={category} 
+              price={price} 
+              image={image} 
+              description={description}
+              loadPlants={loadPlants} />
+            )
+        })
+      }
+      <Toaster />
+      <Link to="/add">
+        <img src={ImgAdd} alt="" className="btn-add"/>
+      </Link>
+    </div> 
   )
 }
 
